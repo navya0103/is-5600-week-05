d08a369b565d33f6c4aa22d11e117a3c830b820f
navya0103 <125131044+navya0103@users.noreply.github.com>
2026-04-19 20:38:23 -0500

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  inStock: Boolean
+});
+
+module.exports = mongoose.model('Product', productSchema);
